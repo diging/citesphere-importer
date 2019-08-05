6888fb9c103526e71d6775bc75c541c2c9a72bd9
Julia Damerow <julia.damerow@googlemail.com>
2019-08-05 11:29:35 -0400

    [CITE-42] more tests

--- citesphere-importer/src/test/java/edu/asu/diging/citesphere/importer/core/service/parse/jstor/xml/TagHandlerTest.java
+++ citesphere-importer/src/test/java/edu/asu/diging/citesphere/importer/core/service/parse/jstor/xml/TagHandlerTest.java
@@ -2,6 +2,8 @@ package edu.asu.diging.citesphere.importer.core.service.parse.jstor.xml;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -28,5 +30,19 @@ public class TagHandlerTest {
     protected Node getNode(String name) {
         NodeList list = doc.getElementsByTagName(name);
         return list.item(0);
+    }
+    
+    protected List<Node> getChildNodes(Node node, String nodeName) {
+        List<Node> nodes = new ArrayList<>();
+        NodeList nodeList = node.getChildNodes();
+        if (nodeList != null) {
+            for (int i=0; i<nodeList.getLength(); i++) {
+                Node childNode = nodeList.item(i);
+                if (childNode.getNodeName().equals(nodeName)) {
+                    nodes.add(childNode);
+                }
+            }
+        }
+        return nodes;
     }
 }
