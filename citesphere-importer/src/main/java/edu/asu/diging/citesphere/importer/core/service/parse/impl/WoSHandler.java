9b95d0eaf61faa66c0558518b0a4ad97b3809ee0
Namratha <Namratha@gbcilsa313grad02imac.dhcp.asu.edu>
2019-09-19 11:36:12 -0700

    [CITE-58] Added support for BOM files.

--- citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/impl/WoSHandler.java
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/impl/WoSHandler.java
@@ -2,12 +2,14 @@ package edu.asu.diging.citesphere.importer.core.service.parse.impl;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
+import org.apache.commons.io.input.BOMInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import edu.asu.diging.citesphere.importer.core.exception.HandlerTestException;
 import edu.asu.diging.citesphere.importer.core.exception.IteratorCreationException;
 import edu.asu.diging.citesphere.importer.core.service.impl.JobInfo;
@@ -26,6 +28,22 @@ public class WoSHandler implements FileHandler {
     @Override
     public boolean canHandle(String path) throws HandlerTestException {
         File file = new File(path);
+        
+        try {
+            BOMInputStream inputStream;
+            inputStream = new BOMInputStream(FileUtils.openInputStream(file), false, ByteOrderMark.UTF_8, 
+                    ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_16LE, ByteOrderMark.UTF_32BE, ByteOrderMark.UTF_32LE);
+        
+            String content;
+            if (inputStream.hasBOM()) {
+                content = IOUtils.toString(inputStream, inputStream.getBOMCharsetName());
+            } else {
+                content = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            }
+            FileUtils.write(file, content, "UTF8");
+        } catch (IOException e1) {
+            throw new HandlerTestException("Unsupported file format.", e1);
+        }
         
         if (path.toLowerCase().endsWith(".txt") && !file.getName().startsWith(".")) {
             try (LineIterator it = FileUtils.lineIterator(file)) {
