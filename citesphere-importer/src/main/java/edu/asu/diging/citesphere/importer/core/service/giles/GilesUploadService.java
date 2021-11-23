7b2b4a90f1f5a2ef42e38545c72862d0dfe85ca0
Charishma0249 <charishma0249@gmail.com>
2021-11-23 14:00:56 -0700

    [CITE-163] Add Giles Implementation

---
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/giles/GilesUploadService.java
@@ -0,0 +1,11 @@
+package edu.asu.diging.citesphere.importer.core.service.giles;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.asu.diging.citesphere.model.bib.IGilesUpload;
+import edu.asu.diging.citesphere.user.IUser;
+
+public interface GilesUploadService {
+
+    IGilesUpload uploadFile(IUser user, MultipartFile file, byte[] fileBytes);
+}
