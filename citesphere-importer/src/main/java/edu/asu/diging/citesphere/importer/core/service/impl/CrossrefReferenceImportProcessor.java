656fab70dad971ee9bdfbfb47656556d410f2a51
Pratik Prakash Giri <32401726+PratikGiri@users.noreply.github.com>
2023-03-06 17:02:27 -0700

    [CITE-177] Trying to add functionality for starting the import

--- citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/impl/CrossrefReferenceImportProcessor.java
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/impl/CrossrefReferenceImportProcessor.java
@@ -11,8 +11,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import edu.asu.diging.citesphere.importer.core.model.BibEntry;
+import edu.asu.diging.citesphere.importer.core.model.ItemType;
 import edu.asu.diging.citesphere.importer.core.service.AbstractImportProcessor;
+import edu.asu.diging.citesphere.messages.model.ItemCreationResponse;
 import edu.asu.diging.citesphere.messages.model.KafkaJobMessage;
+import edu.asu.diging.citesphere.messages.model.ResponseCode;
+import edu.asu.diging.citesphere.messages.model.Status;
 import edu.asu.diging.crossref.exception.RequestFailedException;
 import edu.asu.diging.crossref.model.Item;
 import edu.asu.diging.crossref.service.CrossrefConfiguration;
@@ -38,6 +48,42 @@ public class CrossrefReferenceImportProcessor extends AbstractImportProcessor {
         for (String doi : info.getDois()) {
             try {
                 items.add(crossrefService.get(doi));
+                
+                //
+                ObjectMapper mapper = new ObjectMapper();
+                ArrayNode root = mapper.createArrayNode();
+                int entryCounter = 0;
+                while (items.hasNext()) {
+                    BibEntry entry = bibIterator.next();
+                    if (entry.getArticleType() == null) {
+                        // something is wrong with this entry, let's ignore it
+                        continue;
+                    }
+//                    ItemType type = itemTypeMapping.get(entry.getArticleType());
+//                    JsonNode template = zoteroConnector.getTemplate(type);
+//                    ObjectNode bibNode = generationService.generateJson(template, entry);
+
+                    root.add(bibNode);
+                    entryCounter++;
+
+                    // we can submit max 50 entries to Zotoro
+                    if (entryCounter >= 50) {
+                        submitEntries(root, info);
+                        entryCounter = 0;
+                        root = mapper.createArrayNode();
+                    }
+
+                }
+                
+                bibIterator.close();
+                
+                ItemCreationResponse response = null;
+                if (entryCounter > 0) {
+                    response = submitEntries(root, info);
+                }
+
+                response = response != null ? response : new ItemCreationResponse();
+                sendMessage(response, message.getId(), Status.DONE, ResponseCode.S00);
             } catch (RequestFailedException | IOException e) {
                 logger.error("Couuld not retrieve work for doi: "+ doi, e);
                 // for now we just log the exceptions
