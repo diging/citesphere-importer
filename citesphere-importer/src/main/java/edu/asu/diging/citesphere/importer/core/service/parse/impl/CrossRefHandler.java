10d837208904cfd95a9a5096bdc1b4046b3e360b
Pratik Prakash Giri <32401726+PratikGiri@users.noreply.github.com>
2023-03-10 16:45:58 -0700

    [CITE-177] Trying to create Iterator for Crossref

--- citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/impl/CrossRefHandler.java
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/impl/CrossRefHandler.java
@@ -1,18 +1,7 @@
 package edu.asu.diging.citesphere.importer.core.service.parse.impl;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import org.apache.commons.io.ByteOrderMark;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
-import org.apache.commons.io.input.BOMInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import edu.asu.diging.citesphere.importer.core.exception.HandlerTestException;
@@ -21,60 +10,22 @@ import edu.asu.diging.citesphere.importer.core.service.impl.JobInfo;
 import edu.asu.diging.citesphere.importer.core.service.parse.BibEntryIterator;
 import edu.asu.diging.citesphere.importer.core.service.parse.FileHandler;
 import edu.asu.diging.citesphere.importer.core.service.parse.IHandlerRegistry;
-import edu.asu.diging.citesphere.importer.core.service.parse.iterators.WoSTaggedFieldsIterator;
-import edu.asu.diging.citesphere.importer.core.service.parse.wos.tagged.IArticleWoSTagParser;
+import edu.asu.diging.citesphere.importer.core.service.parse.iterators.CrossRefIterator;
+import edu.asu.diging.citesphere.importer.core.service.parse.jstor.xml.IArticleTagParser;
 
 @Service
 public class CrossRefHandler implements FileHandler {
 
     @Autowired
-    private IArticleWoSTagParser parserRegistry;
+    private IArticleTagParser parserRegistry;
     
     @Value("${_citesphere_download_path}")
     private String downloadPath;
 
     @Override
     public boolean canHandle(String path) throws HandlerTestException {
-//        File file = new File(path);
-//        try {
-//            BOMInputStream inputStream = new BOMInputStream(FileUtils.openInputStream(file), false,
-//                    ByteOrderMark.UTF_8, ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_16LE,
-//                    ByteOrderMark.UTF_32BE, ByteOrderMark.UTF_32LE);
-//            if (inputStream.hasBOM()) {
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-//                File fileCopy = new File(file.getParent() +  File.separator + "CopyOf" + file.getName());
-//                while(reader.ready()) {
-//                    FileUtils.write(fileCopy, reader.readLine(), inputStream.getBOMCharsetName(), true);
-//                }
-//                reader.close();
-//                Files.copy(fileCopy.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-//                Files.deleteIfExists(fileCopy.toPath());
-//            }
-//        } catch (IOException e1) {
-//            throw new HandlerTestException("Unsupported file format.", e1);
-//        }
-
-//        if (path.toLowerCase().endsWith(".txt") && !file.getName().startsWith(".")) {
         if (path == null) {
-//            try (LineIterator it = FileUtils.lineIterator(file)) {
-//                int linesToRead = 10;
-//                int linesRead = 0;
-//
-//                // we check the first 10 lines if they start with two capitals letters
-//                // followed by a space; if they all match, we assume it's WoS' data format.
-//                while (it.hasNext() && linesRead < linesToRead) {
-//                    String line = it.nextLine();
-//                    line = line.replaceAll("\\p{C}", "");
-//                    if (!line.matches("([A-Z0-9]{2}| {2})( .*$|$)") && !line.trim().isEmpty()) {
-//                        return false;
-//                    }
-//                    linesRead++;
-//                }
-
                 return true;
-//            } catch (IOException e) {
-//                throw new HandlerTestException("Could not read lines.", e);
-//            }
         }
         return false;
     }
@@ -82,7 +33,7 @@ public class CrossRefHandler implements FileHandler {
     @Override
     public BibEntryIterator getIterator(String path, IHandlerRegistry callback, JobInfo info)
             throws IteratorCreationException {
-        return new WoSTaggedFieldsIterator(path, parserRegistry);
+        return new CrossRefIterator(path, parserRegistry);
     }
 
 }
--- citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/iterators/CrossRefIterator.java
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/iterators/CrossRefIterator.java
@@ -2,6 +2,8 @@ package edu.asu.diging.citesphere.importer.core.service.parse.iterators;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
@@ -13,109 +15,68 @@ import edu.asu.diging.citesphere.importer.core.model.impl.ArticleMeta;
 import edu.asu.diging.citesphere.importer.core.model.impl.ContainerMeta;
 import edu.asu.diging.citesphere.importer.core.model.impl.Publication;
 import edu.asu.diging.citesphere.importer.core.service.parse.BibEntryIterator;
+import edu.asu.diging.citesphere.importer.core.service.parse.jstor.xml.IArticleTagParser;
 import edu.asu.diging.citesphere.importer.core.service.parse.wos.tagged.IArticleWoSTagParser;
 
 public class CrossRefIterator implements BibEntryIterator {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private IArticleWoSTagParser tagParserRegistry;
+    private IArticleTagParser tagParserRegistry;
     private String filePath;
-    private LineIterator lineIterator;
-    private String currentLine = null;
+    private BibEntry article;
+    
+    private boolean iteratorDone = false;
+    private Map<String, String> typeMap;
 
-    public CrossRefIterator(String filePath, IArticleWoSTagParser parserRegistry) {
+    public CrossRefIterator(String filePath, IArticleTagParser parserRegistry) {
         this.filePath = filePath;
         this.tagParserRegistry = parserRegistry;
         init();
     }
 
     private void init() {
-        try {
-            lineIterator = FileUtils.lineIterator(new File(filePath));
-            if (lineIterator.hasNext()) {
-                // we're at the beginning, so we'll signal that with and empty string
-                currentLine = "";
-            }
-        } catch (IOException e) {
-            logger.error("Could not create line iterator.", e);
-        }
+        typeMap = new HashMap<String, String>();
+        typeMap.put("research-article", Publication.ARTICLE);
+        typeMap.put("book-review", Publication.REVIEW);
+//        parseDocument();
 
     }
 
     @Override
     public BibEntry next() {
-        ArticleMeta articleMeta = new ArticleMeta();
-        ContainerMeta containerMeta = new ContainerMeta();
-
-        BibEntry entry = new Publication();
-        entry.setArticleMeta(articleMeta);
-        entry.setJournalMeta(containerMeta);
-
-        String previousField = null;
-        int fieldIdx = 0;
-
-        while (lineIterator.hasNext()) {
-            String line = lineIterator.nextLine();
-            // this means we are at the end of an entry
-            if (line.trim().isEmpty()) {
-                break;
-            }
-
-            // not a valid line or not filled field
-            if (line.length() < 2) {
-                continue;
-            }
-            String field = line.substring(0, 2);
-            String value = "";
-
-            if (line.length() > 2) {
-                value = line.substring(3);
-            }
-
-            if (field.trim().isEmpty()) {
-                field = previousField;
-                fieldIdx++;
-            } else {
-                fieldIdx = 0;
-            }
-            tagParserRegistry.parseMetaTag(field, value, previousField, fieldIdx, entry, false);
-
-            previousField = field;
+        if (iteratorDone) {
+            return null;
         }
-
-        // in case there are several empty lines between entries
-        // let's skip them
-        advanceToNext();
-
-        return entry;
+        iteratorDone = true;
+        return article;
     }
 
-    private void advanceToNext() {
-        if (lineIterator.hasNext()) {
-            currentLine = lineIterator.next();
-            if (currentLine.trim().isEmpty()) {
-                advanceToNext();
-            }
-        } else {
-            currentLine = null;
-        }
-    }
+//    private void advanceToNext() {
+//        if (lineIterator.hasNext()) {
+//            currentLine = lineIterator.next();
+//            if (currentLine.trim().isEmpty()) {
+//                advanceToNext();
+//            }
+//        } else {
+//            currentLine = null;
+//        }
+//    }
 
     @Override
     public boolean hasNext() {
-        return currentLine != null;
+        return !iteratorDone;
     }
 
     @Override
     public void close() {
-        if (lineIterator != null) {
-            try {
-                lineIterator.close();
-            } catch (IOException e) {
-                logger.error("Couldn't close line iterator.", e);
-            }
-        }
+//        if (lineIterator != null) {
+//            try {
+//                lineIterator.close();
+//            } catch (IOException e) {
+//                logger.error("Couldn't close line iterator.", e);
+//            }
+//        }
     }
 
 }
