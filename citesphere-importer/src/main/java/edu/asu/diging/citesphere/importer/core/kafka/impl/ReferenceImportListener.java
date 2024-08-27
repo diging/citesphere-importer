3a562d531515c3b4fa9e95a12a75a074ba4a0dc2
PradnyaC11 <pchaud21@asu.edu>
2024-08-27 09:47:05 -0700

    [CITE-163] Added collection import kafka topic

--- citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/kafka/impl/ReferenceImportListener.java
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/kafka/impl/ReferenceImportListener.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,7 +19,12 @@ public class ReferenceImportListener {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     
     @Autowired
-    private IImportProcessor processor;
+    @Qualifier("importProcessor")
+    private IImportProcessor fileProcessor;
+    
+    @Autowired
+    @Qualifier("collectionImportProcessor")
+    private IImportProcessor collectionProcessor;
 
     @KafkaListener(topics = KafkaTopics.REFERENCES_IMPORT_TOPIC)
     public void receiveMessage(String message) {
@@ -32,6 +38,22 @@ public class ReferenceImportListener {
             return;
         }
         
-        processor.process(msg);
+        fileProcessor.process(msg);
     }
+    
+    @KafkaListener(topics = KafkaTopics.COLLECTION_IMPORT_TOPIC)
+    public void receiveCollectionImportMessage(String message) {
+        ObjectMapper mapper = new ObjectMapper();
+        KafkaJobMessage msg = null;
+        try {
+            msg = mapper.readValue(message, KafkaJobMessage.class);
+        } catch (IOException e) {
+            logger.error("Could not unmarshall message.", e);
+            // FIXME: handle this case
+            return;
+        }
+        
+        collectionProcessor.process(msg);
+    }
+    
 }
---
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/impl/CollectionImportProcessor.java
@@ -0,0 +1,14 @@
+package edu.asu.diging.citesphere.importer.core.service.impl;
+
+import edu.asu.diging.citesphere.importer.core.service.IImportProcessor;
+import edu.asu.diging.citesphere.messages.model.KafkaJobMessage;
+
+public class CollectionImportProcessor implements IImportProcessor {
+
+    @Override
+    public void process(KafkaJobMessage message) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
