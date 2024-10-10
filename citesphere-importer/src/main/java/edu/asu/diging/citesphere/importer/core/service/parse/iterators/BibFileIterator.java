c05de5cb7aa5280f84fc7b7cb712fce7f8204171
PradnyaC11 <pchaud21@asu.edu>
2024-10-10 12:05:46 -0700

    [CITE-163] Parsed article Type of bib file

--- citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/iterators/BibFileIterator.java
+++ citesphere-importer/src/main/java/edu/asu/diging/citesphere/importer/core/service/parse/iterators/BibFileIterator.java
@@ -1,60 +1,52 @@
 package edu.asu.diging.citesphere.importer.core.service.parse.iterators;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 
-import org.apache.commons.io.LineIterator;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.asu.diging.citesphere.importer.core.model.BibEntry;
+import edu.asu.diging.citesphere.importer.core.model.impl.Publication;
 import edu.asu.diging.citesphere.importer.core.service.parse.BibEntryIterator;
 
 public class BibFileIterator implements BibEntryIterator {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    
+
     private String filePath;
     private Iterator<String> lineIterator;
     private String currentLine = null;
-    
+
     public BibFileIterator(String filePath) {
         System.out.println("------------------ inside bib file iterator");
         this.filePath = filePath;
         init();
     }
-    
+
     private void init() {
-        //        try {
-        //            System.out.println(filePath + " filepath ======================");
-        //            lineIterator = FileUtils.lineIterator(new File(filePath), "UTF-8");
-        //            if (lineIterator.hasNext()) {
-        //                // we're at the beginning, so we'll signal that with and empty string
-        ////                currentLine = "";
-        //                String line = lineIterator.nextLine();
-        //                System.out.println(line + "-------------------------------");
-        //            }
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            lineIterator = reader.lines().iterator();
-
-            //        while (iterator.hasNext()) {
-            //            String line = iterator.next();
-            //            // Process the line
-            //            System.out.println(line);
-            //        }
+        try {
+            System.out.println(filePath + " filepath ======================");
+            lineIterator = FileUtils.lineIterator(new File(filePath), "UTF-8");
         } catch (IOException e) {
             logger.error("Could not create line iterator.", e);
         }
+
+
     }
-    
+
     @Override
     public BibEntry next() {
+        System.out.println(" inside next -------------------------------");
+        BibEntry entry = new Publication();
         while (lineIterator.hasNext()) {
             String line = lineIterator.next();
-            System.out.println(line + " 2 -------------------------------");
+            if(line.contains("@")) {
+                entry.setArticleType(line.substring(line.indexOf('@')+1, line.indexOf('{')));
+                System.out.println("type =========================== " +entry.getArticleType());
+            }
         }
         return null;
     }
@@ -67,7 +59,7 @@ public class BibFileIterator implements BibEntryIterator {
     @Override
     public void close() {
         // TODO Auto-generated method stub
-        
+
     }
 
 }
